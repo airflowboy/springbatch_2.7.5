b87a3b5a7821f09d869c8dcfc1cf1ffcdc90ef01
airflowboy <ajtwoddl9194@gmail.com>
2024-12-20 14:34:16 +0900

    simple batch template

---
+++ src/main/java/com/example/batchtemplate/BatchTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batchtemplate/BatchTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
